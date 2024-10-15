a93cc0fea5ff2ef3534630599f2d4ec4b492d954
Julien <julien.jaeger@kabelmail.de>
2024-10-15 23:37:53 +0200

    with timestamps for train and test and classification report

--- train_kmeans.py
+++ train_kmeans.py
@@ -1,11 +1,10 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.datasets import fetch_openml
-from sklearn.cluster import KMeans
+import time
 import keras
+import numpy as np
+from sklearn.cluster import KMeans
 from sklearn.metrics import classification_report
 
-TOTAL_CLUSTERS = 512
+TOTAL_CLUSTERS = 256
 
 def get_most_common_labels(kmeans, y):
     labels = np.zeros(kmeans.n_clusters)
@@ -22,7 +21,12 @@ x_test = x_test.reshape(10000, 784)
 
 kmeans = KMeans(n_clusters=TOTAL_CLUSTERS, random_state=42)
 
+startTime = time.time()
+
 kmeans.fit(x_train)
+
+endTime = time.time()
+print("time to train: {:.2f}".format(endTime-startTime))
 
 common_labels = get_most_common_labels(kmeans, y_train)
 
@@ -30,5 +34,3 @@ test_clusters = kmeans.predict(x_test)
 test_predictions = common_labels[test_clusters]
 
 print(classification_report(y_test, test_predictions))
-
-# saving the model
